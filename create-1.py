d97a343335afe461b4c12e120dc6b115f3f677b3
YDZ-syj <0X5f010425@gmail.com>
2026-01-28 16:38:06 +0800

    添加计算函数并测试

--- create-1.py
+++ create-1.py
@@ -2,3 +2,12 @@ print("Hello Git")
 def calc(a,b):
     return a+b
 print(calc(1,2))
+print(calc(3,4))
+print(calc(5,6))
+print(calc(7,8))
+print(calc(9,10))
+print(calc(11,12))
+print(calc(13,14))
+print(calc(15,16))
+print(calc(17,18))
+print(calc(19,20))
